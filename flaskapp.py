b422d1c95b9b43784836022a7663be9d98c2a94d
adamehler <adam.ehler@drake.edu>
2025-04-08 15:45:58 -0500

    pretroubleshoot of timeout

--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,7 @@ from dbCode import * #helper functions from dbCode
 
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key' #This came from the example flaskapp.py in class
 #Home page
 
 @app.route("/")
@@ -14,3 +14,6 @@ def index():
     languages = get_most_pop_off_lang()
     #Utilize the template to render the page
     return render_template("official_languages.html", results=languages)
+
+if __name__ == '__main__': #Came from the examples so prob important
+    app.run(host='0.0.0.0', port=8080, debug=True)
