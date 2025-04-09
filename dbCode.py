6c2615aa206644cb87d3d58e582beff210a23639
adamehler <adam.ehler@drake.edu>
2025-04-09 14:59:43 -0500

    add user

--- dbCode.py
+++ dbCode.py
@@ -3,6 +3,11 @@ import pymysql.cursors
 import creds
 import boto3
 
+TABLE_NAME = "Soccer"
+
+dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+table = dynamodb.Table(TABLE_NAME)
+
 # RDS Connection helpers
 
 def get_conn():
@@ -30,3 +35,9 @@ def get_most_pop_off_lang():
     #Returns the 20 most popular official languages in the world in descending order
     query = "select Language, count(Language) FROM countrylanguage WHERE IsOfficial LIKE 'T' GROUP BY Language ORDER BY count(Language) DESC LIMIT 20;"
     return execute_query(query)
+
+def user_add(name, countries):
+    table.put_item{
+        'User': name,
+        'CountriesVisited': countries
+    }
--- flaskapp.py
+++ flaskapp.py
@@ -4,6 +4,7 @@ import creds
 from dbCode import * #helper functions from dbCode
 
 
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' #This came from the example flaskapp.py in class
 #Home page
@@ -14,6 +15,50 @@ def index():
     languages = get_most_pop_off_lang()
     #Utilize the template to render the page
     return render_template("official_languages.html", results=languages)
+
+@app.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        # Extract form data
+        username = request.form['username']
+        countriesVisited = request.form['countriesVisited']
+        user_add(username, countriesVisited)
+
+        
+        # Process the data (e.g., add it to a database)
+        # For now, let's just print it to the console
+        flash('User added successfully!', 'success')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('add_user.html')
+
+@app.route('/delete-user',methods=['GET', 'POST'])
+def delete_user():
+    if request.method == 'POST':
+        # Extract form data
+        name = request.form['name']
+        
+        # Process the data (e.g., add it to a database)
+        # For now, let's just print it to the console
+        print("Name to delete:", name)
+        
+        flash('User deleted successfully!', 'warning')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('delete_user.html')
+
+
+@app.route('/display-users')
+def display_users():
+    # hard code a value to the users_list;
+    # note that this could have been a result from an SQL query :) 
+    users_list = (('John','Doe','Comedy'),('Jane', 'Doe','Drama'))
+    return render_template('display_users.html', users = users_list)
+
 
 if __name__ == '__main__': #Came from the examples so prob important
     app.run(host='0.0.0.0', port=8080, debug=True)
